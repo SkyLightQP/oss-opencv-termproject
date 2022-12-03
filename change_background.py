89d7ad172645a0d44d99659fd51522132f1f9eca
Daegyeom Ha <combbm@gmail.com>
2022-12-04 00:13:21 +0900

    Add change_background() function instead of exists logic

--- change_background.py
+++ change_background.py
@@ -1,11 +1,16 @@
 import cv2
 
+
+def change_background(image):
+    changed_image = cv2.imread('./images/example_image.png')
+    image = changed_image
+    
+    return image
+
+
 # This image is for test. After implementing code of checking a background, change this variable to a background object.
 image = cv2.imread('./images/input.png')
-
-
-changed_image = cv2.imread('./images/example_image.png')
-image = changed_image
+result = change_background(image)
 
 cv2.imshow('Changed Image', image)
 cv2.waitKey()
