7f05773803a8a13b00ac5ce31ce6c58ab38feb12
sgg423 <113335796+sgg423@users.noreply.github.com>
2022-12-06 19:56:12 +0900

    Update facial recognition.py

--- facial recognition.py
+++ facial recognition.py
@@ -7,7 +7,7 @@ def face():
     hogParams = {'winStride': (8,8), 'padding': (32,32), 'scale': 1.05, 'hitThreshold': 0, 'finalThreshold': 5} #파라미터 설정 
 
     #검출
-    img = cv2.imread('img/img01.jpg') #읽기 
+    img = cv2.imread('face.png') #읽기 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #흑백변환 
     human, r = hog.detectMultiScale(gray, **hogParams) #사람 검출 - human에는 사람의 위치정보가 들어감 
 
